1f604b05d3ab15b0af74513950ca59b1a7a1891d
RINLER <90422888+R1NLER@users.noreply.github.com>
2022-08-21 21:12:57 +0200

    Update removejava.sh

--- removejava.sh
+++ removejava.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 dpkg-query -W -f='${binary:Package}\n' | grep -E -e '^(ia32-)?(sun|oracle)-java' -e '^openjdk-' -e '^icedtea' -e '^(default|gcj)-j(re|dk)' -e '^gcj-(.*)-j(re|dk)' -e '^java-common' | xargs sudo apt-get -y remove
 sudo apt-get -y autoremove
 dpkg -l | grep ^rc | awk '{print($2)}' | xargs sudo apt-get -y purge
